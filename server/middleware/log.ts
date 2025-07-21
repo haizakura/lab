3142067e66e33a3ca5564da9eca28cca93e9722f
HAIZAKURA <hhbilly99@gmail.com>
2025-07-21 15:56:15 +0900

    fix: remove useless logger output

--- server/middleware/log.ts
+++ server/middleware/log.ts
@@ -4,7 +4,6 @@ export default defineEventHandler((event) => {
   const { method, url, headers } = event.node.req;
   const userAgent = headers['user-agent'] || 'Unknown';
   const ip = headers['x-forwarded-for'] || 'Unknown';
-  const host = headers['host'] || 'Unknown';
 
-  logger.info(`[${method}] ${url} - IP: ${ip} - Host: ${host} - User-Agent: ${userAgent}`);
+  logger.info(`[${method}] ${url} - IP: ${ip} - User-Agent: ${userAgent}`);
 });
