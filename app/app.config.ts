4d433aff7fb598b8af38c78f3c7f56721e720ee7
HAIZAKURA <hhbilly99@gmail.com>
2025-07-21 15:39:42 +0900

    feat: project item config init

---
+++ app/app.config.ts
@@ -0,0 +1,40 @@
+export default defineAppConfig({
+  itemConfig: {
+    rate: {
+      title: '💰 Exchange Rate Query',
+      name: 'rate',
+      path: '/rate',
+      desc: 'Exchange rates provided on this website are from UnionPay System.',
+    },
+    longTextMaker: {
+      title: '🔤 Long Text Maker',
+      name: 'longTextMaker',
+      path: '/long-text-maker',
+      desc: 'Make long text with ease.',
+    },
+    item01: {
+      title: '⚒️ Under Development',
+      name: 'underDevelopment',
+      path: '/under-development',
+      desc: 'This project is under development.',
+    },
+    item02: {
+      title: '⚒️ Under Development',
+      name: 'underDevelopment',
+      path: '/under-development',
+      desc: 'This project is under development.',
+    },
+    item03: {
+      title: '⚒️ Under Development',
+      name: 'underDevelopment',
+      path: '/under-development',
+      desc: 'This project is under development.',
+    },
+    item04: {
+      title: '⚒️ Under Development',
+      name: 'underDevelopment',
+      path: '/under-development',
+      desc: 'This project is under development.',
+    },
+  },
+});
