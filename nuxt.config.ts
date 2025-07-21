f2e35170750bc9171d4b6fe5211b8ec371f2bf6f
HAIZAKURA <hhbilly99@gmail.com>
2025-07-21 15:37:05 +0900

    chore: add site title & meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,27 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  modules: ['@element-plus/nuxt'],
+  css: ['@/assets/style.scss'],
+  vite: {
+    ssr: {
+      noExternal: ['log4js', 'dayjs'],
+    },
+    optimizeDeps: {
+      include: ['dayjs'],
+    },
+  },
+  app: {
+    head: {
+      title: 'HAIZAKURA Lab',
+      meta: [
+        { name: 'description', content: 'HAIZAKURA Lab' },
+        { name: 'keywords', content: 'HAIZAKURA, Lab, Tools, Exchange Rate' },
+        { name: 'author', content: 'HAIZAKURA' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        { charset: 'utf-8' },
+      ],
+      noscript: [{ textContent: 'JavaScript is required' }],
+    },
+  },
 });
