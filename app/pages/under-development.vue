a149b3951c12e1d02f4c775b520ba55a47522903
HAIZAKURA <hhbilly99@gmail.com>
2025-07-21 15:37:32 +0900

    chore: add under development page

---
+++ app/pages/under-development.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="under-development">
+    <h1>Under Development</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  name: 'underDevelopment',
+});
+</script>
+
+<style lang="scss" scoped></style>
