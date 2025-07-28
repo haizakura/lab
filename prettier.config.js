e3d90ce71798229d71cdacc99bde8a2af8959a53
HAIZAKURA <hhbilly99@gmail.com>
2025-07-28 23:56:58 +0900

    chore: use prettier formatter

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  vueIndentScriptAndStyle: true,
+};
+
+export default config;
